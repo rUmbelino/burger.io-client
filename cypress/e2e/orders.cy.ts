b85cc49a12803228180c6fca01fced828e8b33de
Rafael Ferreira Umbelino <rafael.umbelino@encora.com>
2023-12-06 16:19:37 -0300

    creating orders test spec

---
+++ cypress/e2e/orders.cy.ts
@@ -0,0 +1,24 @@
+import orders from '../fixtures/orders.json';
+
+describe('orders spec', () => {
+	beforeEach(() => {
+		cy.intercept('POST', `${cy.getApiPath()}/order`, { statusCode: 200, body: orders });
+
+		cy.visit('/orders');
+		cy.wait(500);
+	});
+
+	it('renders the list of orders', () => {
+		orders.forEach(({ id, recepies }) => {
+			cy.get('.accordion-button').should('have.text', `Pedido #${id}`);
+
+			recepies.forEach(({ icon, quantity, name }, i) => {
+				cy.get('[data-cy="icon-element"] i').eq(i).should('have.text', icon);
+				cy.get('[data-cy="icon-element"] span').eq(i).should('have.text', `${quantity}x - ${name}`);
+			});
+
+			cy.get('button').contains('Pedir Novamente').click();
+			cy.get('.modal-content').should('exist');
+		});
+	});
+});
